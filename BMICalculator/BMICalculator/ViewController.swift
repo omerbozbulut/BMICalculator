8dd81b0b95d07f29147fd2dbf49804ef48bc9da5
omer <omerbzblt613@gmail.com>
2022-03-01 00:14:45 +0300

    get UISlider data

--- BMICalculator/BMICalculator/ViewController.swift
+++ BMICalculator/BMICalculator/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var HeightLabel: UILabel!
+    
+    @IBOutlet weak var WeightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func HeightSliderChanged(_ sender: UISlider) {
+        HeightLabel.text=String(format: "%.2f", sender.value)+"m"
+    }
+    
+    @IBAction func WeightSliderChanged(_ sender: UISlider) { WeightLabel.text=String(format:"%.0f", (sender.value*100))+"Kg"
+    }
+    
+    
+    
+    
 }
 
