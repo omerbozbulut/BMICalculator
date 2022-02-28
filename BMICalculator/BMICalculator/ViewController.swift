b3bcc891cdd3397ef82c4d1f9231bdc076c10190
omer <omerbzblt613@gmail.com>
2022-02-28 21:09:10 +0300

    Design complete

---
+++ BMICalculator/BMICalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by omer faruk bozbulut on 20.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
